095927faba69b994b74cda119de20a1340ef011e
Arnau <arnausatorra@gmail.com>
2025-04-30 20:15:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//TODO 1- Pide al usuario dos números. Muestra el resultado de la resta de ambos, en pantalla y/o en consola
+
+const restaEnteros = () => {
+    let a = parseInt(prompt("Introduce el primer numero"));
+    let b = parseInt(prompt("Introduce el segundo numero"));
+    alert("El resultado de la resta es: " + (a - b));
+};
+
+restaEnteros();
+
+//TODO 2- Pide al usuario que introduzca nombre, localidad donde vive y hobby. Que se muestre en una sola frase: "Te llamas XXX, vives en YYY, y te gusta ZZZ".
+
+
+
+//TODO 3- Pedir el radio al usuario para calcular el área de un círculo. Buscad la fórmula para calcular la superficie del círculo y el valor de PI (con 4 decimales). 
