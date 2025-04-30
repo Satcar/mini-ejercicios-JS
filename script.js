f057be752487b2d14a3ed2273132cd9a1fdbd29c
Arnau <arnausatorra@gmail.com>
2025-04-30 20:31:45 +0200

    lab finished

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ const restaEnteros = () => {
     alert("El resultado de la resta es: " + (a - b));
 };
 
-//LLAMAR FUNCION
-restaEnteros();
 
 //TODO 2- Pide al usuario que introduzca nombre, localidad donde vive y hobby. Que se muestre en una sola frase: "Te llamas XXX, vives en YYY, y te gusta ZZZ".
 
@@ -30,7 +28,24 @@ const datosUsuario = () => {
     alert(`Te llamas ${nombre}, vives en ${localidad}, y te gusta ${hobby}`);
 };
 
-//LLAMAR FUNCION
-datosUsuario();
 
 //TODO 3- Pedir el radio al usuario para calcular el área de un círculo. Buscad la fórmula para calcular la superficie del círculo y el valor de PI (con 4 decimales). 
+
+const calcularRadio = () => {
+
+    //DECLARACION CONSTANTES Y VARIABLES
+    const PI = Math.PI;
+    let radio = prompt("Introduce el radio del area que desees calcular");
+
+    //LOGICA:: FORMULA AREA: PI x RADIO^2
+    let area = parseInt(PI * radio ** 2); //el operador ** equivale a "elevado a". Muestro el resultado aproximado en un valor entero para que no ocupe tanto espacio
+
+    //MOSTRAR RESULTADO
+    alert(`El área de un círculo con radio ${radio} es: ${area}`);
+
+};
+
+//LLAMAR FUNCIONES
+restaEnteros();
+datosUsuario();
+calcularRadio();
